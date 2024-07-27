e8d621a6d811dd999a6c274461aaba4a5fbdcae0
Steven Li <listeven6691@gmail.com>
2024-07-27 15:11:31 -0400

    Added hash location strategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppComponent } from './app.component';
 import { TicTacToeComponent } from './tic-tac-toe/tic-tac-toe.component';
 import { AppRoutingModule } from './app-routing.component';
 import { ConnectFiveComponent } from './connect-five/connect-five.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
+const routes: Routes = [
+  { path: 'tic-tac-toe', component: TicTacToeComponent },
+  { path: 'connect-five', component: ConnectFiveComponent },
+  { path: '', redirectTo: '/tic-tac-toe', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [AppComponent, TicTacToeComponent, ConnectFiveComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
